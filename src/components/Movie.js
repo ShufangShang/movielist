e1bad176f82e0f379b10242d2188b53728501e60
Shufang Shang <shufang.x.shang@gmail.com>
2023-03-10 00:57:40 -0600

    Add button working now

---
+++ src/components/Movie.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+import '../css/Movie.css';
+
+class Movie extends Component {
+  constructor(props) {
+    super(props);
+    this.state = ({
+      title: this.props.title,
+      description: this.props.prescription,
+      year: this.props.year,
+      genre: this.props.genre,
+      editMode: false
+    });
+  }
+
+
+
+  render() {
+    return (
+      <div className="card-group col-sm-4">
+        <div className="card card-view mt-3">        
+          <div className="card-body">
+            <h5 className="card-title h-10">{this.props.title}</h5> {/*Movie Title*/}
+            <p ClassName="card-text h-40">{this.props.description}</p> {/*Movie Description*/}
+            <ul class="list-group list-group-flush">
+               <li class="list-group-item h-10">Year: {this.props.year}</li> {/*Year of production*/}
+               <li class="list-group-item h-10">{this.props.genre}</li> {/*Genre*/}
+            </ul>
+            
+            <div class="card-body h-10">
+              <button className="btn btn-info mr-1 p-1">Edit</button>
+              <button className="btn btn-danger mr-1 p-1">Delete</button>
+              <button className="btn btn-primary p-1">Preview</button>
+            </div>         
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Movie;
+
---
+++ src/components/Movies.js
@@ -0,0 +1,63 @@
+import React, {Component} from 'react';
+import '../css/Movies.css';
+import Movie from './Movie';
+
+class MovieCollection extends Component {
+  constructor() {
+    super();
+    this.state =
+    {
+        movies: [
+          {title: "Hard Boiled", decription: "Hard Boiled is a 1992 Hong Kong action thriller film.", year: "1992", genre: "Action"},
+          {title: "he Flight of Dragons", decription: "The Flight of Dragons is animated fantasy film.", year: "1982", genre: "Animation"},
+          {title: "Charlie’s Angels", decription: "A team of female private agents, popularly known as Charlie’s Angels.", year: "2000", genre: "Comedy"}, 
+          {title: "Ocean’s Eleven", decription: "Danny Ocean, a gangster, assembles a group of eleven people in an effort to steal money from three popular casinos.", year: "2001", genre: "Fantasy"},
+          {title: "Movie 5", decription: "Movie 5 description.", year: "2005", genre: "Fantasy"},
+          {title: "Movie 6", decription: "Movie 6 description.", year: "2020", genre: "Fantasy"}   
+              
+        ]     
+    };
+
+  }
+
+  // MoviesCollection.js MoviesCollection Component Class Function
+
+  addMovie() {
+    this.state.movies.push(
+      {
+        title: "New Movie Title",
+        description: "New Movie Description",
+        year: "New Movie year",
+        gener: "New Movie Genre"
+      }
+    );
+    this.setState(
+      {
+        movies: this.state.movies
+      }
+    );
+  }
+  
+
+  render() {
+    return (
+      <div>
+        <div className="div-movies">
+          <div className="row">
+            {
+               this.state.movies.map(movie => {
+                return <Movie title={movie.title} decription={movie.description} year={movie.year} gener={movie.genre}/>
+              })
+            }
+          </div>
+        </div>
+        <div>
+          <button className="btn btn-success add-button" onClick={this.addMovie.bind(this)}>Add</button>
+        </div>
+      </div>
+    );
+  } 
+  
+} 
+
+export default MovieCollection;
